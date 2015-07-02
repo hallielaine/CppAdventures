825e6d990c1d55e63515e60b4474ce94fcda37a8
Keyblade88 <Keyblade88@gmail.com>
2015-07-01 17:19:02 -0700

    Create stack.hpp
    header file for stack problem

---
+++ 03-stack/stack.hpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h> 
+#include <stdlib.h> 
+
+class Stack {
+    public:
+        Stack();
+        ~Stack();
+        
+        // Tests whether or not the stack is empty
+        bool Empty();
+        //Gets the size of the stack
+        int Size();
+        // Get the element from the top of the Stack
+        int Top();
+        //Add an element to the top of the Stack
+        void Push(int value);
+        // Remove the element fromt he top of the Stack
+        int Pop();
+        
+    private:
+        int *data;
+        int size;
+};
